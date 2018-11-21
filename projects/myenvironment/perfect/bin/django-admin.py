0d748e75ae13a2395f01a9054628ab240643e8f0
Abhishek Kalhan <abhishek.kalhan@gmail.com>
2018-11-21 14:58:54 +0530

    Added Project

---
+++ projects/myenvironment/perfect/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/abhishek/Downloads/Django-Shopping-Cart-master/projects/myenvironment/perfect/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
